d0ab8df9bcfcc66725655d729c7f07a108c1eded
Quang (Waang) <81631701+orgball2608@users.noreply.github.com>
2023-05-23 22:21:30 +0700

    Add Database Schema

--- data.sql
+++ data.sql
@@ -1,4 +1,13 @@
 drop table if exists users;
+drop table if exists categories;
+drop table if exists products;
+drop table if exists category_products;
+drop table if exists product_ratings;
+drop table if exists orders;
+drop table if exists order_details;
+drop table if exists contacts;
+drop table if exists carts;
+drop table if exists cart_products;
 drop table if exists images;
 
 create table users
@@ -9,7 +18,7 @@ create table users
     last_name  varchar(50)                                                                  not null,
     first_name varchar(50)                                                                  not null,
     phone      varchar(20)                                                                  null,
-    role      enum ('user', 'admin') default 'user'                                        not null,
+    role       enum ('user', 'admin') default 'user'                                        not null,
     salt       varchar(50)                                                                  null,
     avatar     json                                                                         null,
     status     int                    default 1                                             not null,
@@ -19,9 +28,133 @@ create table users
 );
 
 
+create table categories
+(
+    id          int auto_increment primary key,
+    name        varchar(100)                                                    not null,
+    description text                                                            null,
+    icon        json                                                            null,
+    status      int       default 1                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null,
+    total_product int     default 0                                             not null
+);
+
+
+create table products
+(
+    id          int auto_increment primary key,
+    name        varchar(255)                                                    not null,
+    description text                                                            null,
+    price       float                                                           not null,
+    quantity    int                                                             not null,
+    images      json                                                            not null,
+    status      int       default 1                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null,
+    total_rating int     default 0                                             not null
+);
+
+create table category_products
+(
+    category_id   int                                                             not null,
+    product_id int                                                             not null,
+    status        int       default 1                                                 not null,
+    created_at    timestamp default current_timestamp                             null,
+    updated_at    timestamp default current_timestamp on update current_timestamp null,
+    primary key (category_id,  product_id),
+    index (product_id)
+);
+
+create table product_ratings
+(
+    id         int auto_increment primary key,
+    user_id    int                                                             not null,
+    product_id    int                                                             not null,
+    point      float     default 0                                             null,
+    comment    text                                                            null,
+    status     int       default 1                                             not null,
+    created_at timestamp default current_timestamp                             null,
+    updated_at timestamp default current_timestamp on update current_timestamp null
+);
+
+create table orders
+(
+    id          int auto_increment primary key,
+    user_id     int                                                             not null,
+    total_price float                                                           not null,
+    status      int       default 1                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null
+);
+
+
+create table order_details
+(
+    id          int auto_increment primary key,
+    order_id    int                                                             not null,
+    product_origin json                                                            null,
+    price       float                                                           not null,
+    quantity    int                                                             not null,
+    discount    float     default 0                                             null,
+    status      int       default 1                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null
+);
+
+
+create table contacts (
+                      id int auto_increment primary key,
+                      name varchar(255) not null,
+                      addr varchar(255) not null,
+                      phone varchar(20) null,
+                      comment text null,
+                      status int default 1 not null,
+                      created_at timestamp default current_timestamp null,
+                      updated_at timestamp default current_timestamp on update current_timestamp null
+);
+
+create table carts
+(
+    id          int auto_increment primary key,
+    user_id        int                                                             not null,
+    status      int       default 1                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null,
+    total_product int     default 0                                             not null
+);
+
+create table cart_products
+(
+    cart_id          int auto_increment primary key,
+    product_id        int                                                             not null,
+    quantity    int                                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null
+);
+
+
+create table favorites
+(
+    id          int auto_increment primary key,
+    user_id        int                                                             not null,
+    status      int       default 1                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null,
+    total_product int     default 0                                             not null
+);
+
+create table favorite_products
+(
+    favorite_id          int auto_increment primary key,
+    product_id        int                                                             not null,
+    created_at  timestamp default current_timestamp                             null,
+    updated_at  timestamp default current_timestamp on update current_timestamp null
+);
+
 create table images (
-    id int auto_increment primary key,
-    url text not null,
-    width int not null,
-    height int not null
+                        id int auto_increment primary key,
+                        url text not null,
+                        width int not null,
+                        height int not null
 );
