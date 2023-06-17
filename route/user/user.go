22863f116ebbe2a76a43e9ba0942ee83214905b3
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-06-17 16:21:26 +0700

    Fix Update Api With Permission

--- route/user/user.go
+++ route/user/user.go
@@ -2,7 +2,8 @@ package user
 
 import (
 	"TKPM-Go/component/appctx"
-	"TKPM-Go/middleware"
+	"TKPM-Go/module/category/categorytransport/gincategory"
+	"TKPM-Go/module/product/producttransport/ginproduct"
 	"TKPM-Go/module/upload/uploadtransport/ginupload"
 	"TKPM-Go/module/user/usertransport/ginuser"
 
@@ -12,9 +13,17 @@ import (
 func UserRoute(appContext appctx.AppContext, v1 *gin.RouterGroup) {
 	v1.POST("/register", ginuser.Register(appContext))
 	v1.POST("/authenticate", ginuser.Login(appContext))
-	v1.GET("/refresh", middleware.RequireAuth(appContext), ginuser.RefreshToken(appContext))
-	v1.GET("/profile", middleware.RequireAuth(appContext), ginuser.GetProfile(appContext))
+
 	//upload service
 	v1.POST("/upload", ginupload.Upload(appContext))
-	v1.DELETE("/remove/:id", ginupload.Remove(appContext))
+
+	//Category
+	category := v1.Group("/categories")
+	category.GET("/:id", gincategory.GetCategory(appContext))
+	category.GET("/", gincategory.ListCategory(appContext))
+
+	//product
+	product := v1.Group("/products")
+	product.GET("/:id", ginproduct.GetProduct(appContext))
+	product.GET("/", ginproduct.ListProduct(appContext))
 }
