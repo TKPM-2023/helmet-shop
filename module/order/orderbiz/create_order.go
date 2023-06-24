f06c6e90d23e5a20f0816b155a5cdd9a15e066f5
handanbadotcom <cd.synguyen@gmail.com>
2023-06-24 22:52:56 +0700

    update order

--- module/order/orderbiz/create_order.go
+++ module/order/orderbiz/create_order.go
@@ -3,6 +3,8 @@ package orderbiz
 import (
 	"TKPM-Go/common"
 	"TKPM-Go/module/order/ordermodel"
+	"TKPM-Go/module/order_detail/orderdetailbiz"
+	"TKPM-Go/module/product/productbiz"
 	"context"
 )
 
@@ -15,31 +17,47 @@ type CreateOrderStore interface {
 }
 
 type createOrderBusiness struct {
-	store CreateOrderStore
+	createorder_store CreateOrderStore
+	product_store	productbiz.GetProductStore
+	orderdetail_store orderdetailbiz.CreateOrderDetailStore
 }
 
-func NewCreateOrderBusiness(store CreateOrderStore) *createOrderBusiness {
-	return &createOrderBusiness{store: store}
+func NewCreateOrderBusiness(createorder_store CreateOrderStore,
+							product_store	productbiz.GetProductStore,
+							orderdetail_store orderdetailbiz.CreateOrderDetailStore) *createOrderBusiness {
+	return &createOrderBusiness{createorder_store: createorder_store,
+								product_store: product_store,
+								orderdetail_store: orderdetail_store,
+	}
 }
 
 func (business *createOrderBusiness) CreateOrder(context context.Context, data *ordermodel.OrderCreate) error {
 	if err := data.Validate(); err != nil {
 		return err
 	}
-
-	/*
-	_, err := business.store.FindOrderWithCondition(context, map[string]interface{}{"user_id": data.User_ID})
-
-	if err != nil {
-		return err
-	}
-
-	if result != nil {
-		return common.ErrEntityExisted(ordermodel.EntityName, nil)
-	}
-	*/
-	if err := business.store.CreateOrder(context, data); err != nil {
+	if err := business.createorder_store.CreateOrder(context, data); err != nil {
 		return common.ErrCannotCreateEntity(ordermodel.EntityName, err)
+	}
+
+	for i := range data.Products {
+		data.Products[i].Order_ID = data.Id
+		//get product info from model products
+		//product, err := business.product_store.GetProduct(context.Request.Context(), int(data.Products[i].Product_Origin.UID.GetLocalID()))
+		product, err := business.product_store.FindProductWithCondition(context, map[string]interface{}{"id": data.Products[i].Product_Origin.UID.GetLocalID()})
+
+
+		if err != nil {
+			panic(err)
+		}
+
+		//assign to prodcut_origin
+		data.Products[i].Product_Origin.Description = product.Description
+		data.Products[i].Product_Origin.Name = product.Name
+		data.Products[i].Price = (float64(product.Price) * float64(data.Products[i].Quantity)) - (float64(product.Price) * float64(data.Products[i].Discount))
+
+		if err := business.orderdetail_store.CreateOrderDetail(context, &data.Products[i]); err != nil {
+			panic(err)
+		}
 	}
 	return nil
 }
--- module/order/ordertransport/ginorder/create_order.go
+++ module/order/ordertransport/ginorder/create_order.go
@@ -3,14 +3,12 @@ package ginorder
 import (
 	"TKPM-Go/common"
 	"TKPM-Go/component/appctx"
-	"TKPM-Go/module/contact/contactbiz"
-	"TKPM-Go/module/contact/contactstorage"
 	"TKPM-Go/module/order/orderbiz"
 	"TKPM-Go/module/order/ordermodel"
 	"TKPM-Go/module/order/orderstorage"
-	"TKPM-Go/module/order_detail/orderdetailbiz"
+//	"TKPM-Go/module/order_detail/orderdetailbiz"
 	"TKPM-Go/module/order_detail/orderdetailstorage"
-	"TKPM-Go/module/product/productbiz"
+//	"TKPM-Go/module/product/productbiz"
 	"TKPM-Go/module/product/productstorage"
 	"net/http"
 
@@ -29,51 +27,17 @@ func CreateOrder(appCtx appctx.AppContext) gin.HandlerFunc {
 
 		data.User_ID = requester.GetUserId() //int(data.User_UID.GetLocalID())
 		data.Contact_ID = int(data.Contact_UID.GetLocalID())
-		store := orderstorage.NewSQLStore(db)
-		business := orderbiz.NewCreateOrderBusiness(store)
 
-		contact_store := contactstorage.NewSQLStore(db)
-		contact_business := contactbiz.NewGetContactBusiness(contact_store)
-		if _,err := contact_business.GetContact(context.Request.Context(), data.Contact_ID); err!=nil {
-			panic(err)
-		}
+		store := orderstorage.NewSQLStore(db)
+		product_store := productstorage.NewSQLStore(db)
+		orderdetail_store := orderdetailstorage.NewSQLStore(db)
+		business := orderbiz.NewCreateOrderBusiness(store,product_store, orderdetail_store)
 
 		if err := business.CreateOrder(context.Request.Context(), &data); err != nil {
 			panic(err)
 		}
-
+		
 		data.Mask()
-
-		//fetch product_origin
-
-		//Create store
-		product_store := productstorage.NewSQLStore(db)
-		product_business := productbiz.NewGetProductBusiness(product_store)
-
-		orderdetail_store := orderdetailstorage.NewSQLStore(db)
-		orderdetail_business := orderdetailbiz.NewCreateOrderDetailBusiness(orderdetail_store)
-
-		//for each Products
-		for i := range data.Products {
-			data.Products[i].Order_UID = data.FakeId
-			data.Products[i].Order_ID = int(data.Products[i].Order_UID.GetLocalID())
-
-			//get product info from model products
-			product, err := product_business.GetProduct(context.Request.Context(), int(data.Products[i].Product_Origin.UID.GetLocalID()))
-
-			if err != nil {
-				panic(err)
-			}
-
-			//assign to prodcut_origin
-			data.Products[i].Product_Origin.Description = product.Description
-			data.Products[i].Product_Origin.Name = product.Name
-			data.Products[i].Price = (float64(product.Price) * float64(data.Products[i].Quantity)) - (float64(product.Price) * float64(data.Products[i].Discount))
-
-			if err := orderdetail_business.CreateOrderDetail(context.Request.Context(), &data.Products[i]); err != nil {
-				panic(err)
-			}
-		}
 		context.JSON(http.StatusCreated, common.SimpleSuccessResponse(data.FakeId.String()))
 
 	}
